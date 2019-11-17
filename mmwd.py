1a8339d5a696d75a21a5d01c9b4ad80321a14c07
Mateusz <twgg56@gmail.com>
2019-11-17 13:58:43 +0100

    dokonczeni funkcji kosztow
    szkic TABU search

--- mmwd.py
+++ mmwd.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 
 inf = float('nan')
 
@@ -13,7 +14,8 @@ bin_locations =  [[inf, 3,   6,   8,   10   ],
 
 #print(bin_location)
 
-rubbish_in_location = [ 1, 1, 1, 1,1]   # ilosc smieci od kazdego miasta
+rubbish_in_location = [0, 50, 10, 50, 40,90]   # ilosc smieci od kazdego miasta
+                                                # index 0 baza
 
 trucks_volume = [50, 100, 100] # pojemnosci
 trucks_filled_volume = [0] * len(trucks_volume)
@@ -21,9 +23,9 @@ print(len(list(range(0,5))))
 
 # rozdziel po rowno
 # przyjmuje globalne bin_location, garbage_trucks
-# zwraca poczatkowe sollution
-def first_sollotion( location: List, trucks:List )-> List:
-    sollution = [0]*len(trucks)
+# zwraca poczatkowe solution
+def first_solution( location: List, trucks:List )-> List:
+    solution = [0]*len(trucks)
     bins_amount = int((len(location)-1) / len(trucks))
     rest = (len(location)-1) % len(trucks)
     _from = 1
@@ -33,20 +35,17 @@ def first_sollotion( location: List, trucks:List )-> List:
         if rest != 0:
             _to +=1
             rest -= 1
-        sollution[i] = list(range(_from,_to))
+        solution[i] = list(range(_from,_to))
         _from = _to
-
-    print(sollution)
-    return sollution
+    print(solution)
+    return solution
 
 # funkcja kosztu dla sollution
-def count_cost(sollution:List):
+def count_cost(solution:List):
     cost = 0
-    for num_truck in range (0,len(sollution)):  #-1
-        #print(sollution[num_truck])
-        cost += truck_ride_cost(sollution[num_truck],num_truck)
-        #print(cost)
-
+    for num_truck in range (0,len(solution)):
+        cost += truck_ride_cost(solution[num_truck],num_truck)
+        print(cost)
     return cost
 
 def truck_ride_cost(locations:List,num_truck:int): # zwraca koszt dla jednej śmieciarki
@@ -54,22 +53,61 @@ def truck_ride_cost(locations:List,num_truck:int): # zwraca koszt dla jednej śm
     ride_cost = bin_locations[0][locations[0]]
 
     for i in range(0, len(locations)):
-        print("cost ", ride_cost)
-        #trucks_filled_volume[num_truck] += rubbish_in_location[locations[i]]# zaladowanie smieci
+
+        trucks_filled_volume[num_truck] += rubbish_in_location[locations[i]]# zaladowanie smieci
+        print(trucks_filled_volume)
         if( i+1 >= len(locations)): # jesli ostatni
             ride_cost += bin_locations[locations[i]][0]
-            print("if",ride_cost)
             return ride_cost
 
-        if (trucks_volume[num_truck] < trucks_filled_volume[num_truck] + rubbish_in_location[i + 1]): # wroc do bazy jesli smieciarka pelna
-            ride_cost += bin_locations[i][0]
-            return ride_cost
+        if (trucks_volume[num_truck] <= trucks_filled_volume[num_truck] + rubbish_in_location[i + 1]): # wroc do bazy jesli smieciarka pelna
+            ride_cost += bin_locations[locations[i]][0]
+            ride_cost += bin_locations[0][locations[i+1]]
 
         else:
             ride_cost += bin_locations[ locations[i] ][ locations[i + 1] ]
 
     return ride_cost
 
-#sollution = first_sollotion(list(range(0,20)),list(range(0,6)))
-sollution = first_sollotion(bin_locations,trucks_volume )
-count_cost(sollution)
+first_solution(list(range(0,20)),list(range(0,4)))
+solution = first_solution(bin_locations, trucks_volume)
+count_cost(solution)
+
+## dotad jest ok
+
+# przuklad TABU list
+TABU_list = [(1,10),
+             (3,2),
+             (4,5)
+             ]
+
+
+#tabu search
+x = solution
+x_opt = solution
+
+for i in range (0,20):
+    while(1)# zmieniaj dopoki x jest zabronione
+        x = change_solution(x)
+        if(check(x))
+            break
+
+    if count_cost(x) < count_cost(x_opt):
+        x_opt = x
+
+    # dekrementu drugi element krotki = skroc o 1 kadencje
+    # Dodaj nowe elementy do listy TABU
+    # jesli kadnecja = 0 to usun zabronienie
+
+
+
+def check_solution(x:List)->bool:
+    return 1
+
+def change_solution(x:List)->List:
+    new_solution = []
+    return new_solution
+
+def add_to_TABU():
+    TABU_list.append((3, 10))
+    return None
