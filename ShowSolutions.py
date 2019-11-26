a44ecdc8c295eb917e52d2c4e12d9af2760aea17
WasuMrTomass0 <39247298+WasuMrTomass0@users.noreply.github.com>
2019-11-26 12:57:17 +0100

    Dodana nowa opcja wyświetlania rozwiązania
    Możliwe 2 opcje rysowania: strzałki lub zwykłe linie

--- ShowSolutions.py
+++ ShowSolutions.py
@@ -3,12 +3,13 @@ from copy import deepcopy
 import matplotlib.pyplot as plt
 
 
-def show_routes(routes_lists: List, xy_points: List, colours: List[str] = None,
+def show_routes(routes_lists: List, xy_points: List, colours: List[str] = None, arrow: bool = True,
                 point_marker: str = None, point_color: str = None, separate_plots: bool = False) -> None:
     ###
     # routes_lists - rozwiązanie zadania. Lista list (tras śmieciarek) -> to powinny być indeksy punktow w liscie :)
     # xy_points - wylosowane punkty (Wspolrzedne XY) jako krotki w liscie
     # colours - lista kolejnych kolorow (jezeli chcemy, żeby była narzucona)
+    # arrow - Czy rysowac strzalki? Jesli nie zostana zwykle linie
     # point_marker - jakim symbolem maja byc zaznaczane punkty
     # point_color - kolor punktu
     # separate_plots - Czy chcemy mieć osobne wykresy dla każdej śmieciarki?
@@ -17,7 +18,7 @@ def show_routes(routes_lists: List, xy_points: List, colours: List[str] = None,
     if colours is None:
         colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'maroon', 'orangered', 'orange', 'lime']
     if point_marker is None:
-        point_marker = '.'
+        point_marker = 'o'
     if point_color is None:
         point_color = 'r'
 
@@ -25,9 +26,8 @@ def show_routes(routes_lists: List, xy_points: List, colours: List[str] = None,
     # _y = [xy[1] for xy in xy_points]  # Wyciągnięte wartości
 
     lista_legendy = []
-
     for i in range(len(routes_lists)):  # Petla po kazdej smieciarce
-        # lista_legendy.append("ID pojazdu: " + str(i))  # TODO Dodac ewentualne ID pojazdu, jak beda rozrozniane
+        lista_legendy.append("ID pojazdu: " + str(i))  # TODO Dodac ewentualne ID pojazdu, jak beda rozrozniane
         _x_route = [xy_points[xyID][0] for xyID in routes_lists[i]]  # Punkty, ktore odwiedza dana smieciarka
         _y_route = [xy_points[xyID][1] for xyID in routes_lists[i]]
 
@@ -39,18 +39,28 @@ def show_routes(routes_lists: List, xy_points: List, colours: List[str] = None,
         color_helper = colours[helper]
         del helper
 
-        plt.plot(_x_route[:2], _y_route[:2], c=color_helper, linestyle=':')  # Wyjazd z bazy do pierwszego punktu
-        plt.plot(_x_route[1:len(_x_route)-1], _y_route[1:len(_y_route)-1], c=color_helper,  # Trasa
-                 linestyle='-', label=('ID pojazdu: '+str(i)))
-        plt.plot(_x_route[len(_x_route)-2:], _y_route[len(_y_route)-2:], c=color_helper, linestyle='-.')  # Powrot
+        if arrow:
+            for a in range(len(routes_lists[i])-1):
+                _dx = _x_route[a+1] - _x_route[a]
+                _dy = _y_route[a + 1] - _y_route[a]
+                plt.arrow(_x_route[a], _y_route[a], _dx, _dy, head_width=1.2, head_length=2.5,
+                          fc=color_helper, ec=color_helper, color=color_helper, length_includes_head=True)
+        else:
+            plt.plot(_x_route[:2], _y_route[:2], c=color_helper, linestyle=':')  # Wyjazd z bazy do pierwszego punktu
+            plt.plot(_x_route[1:len(_x_route)-1], _y_route[1:len(_y_route)-1], c=color_helper,  # Trasa
+                     linestyle='-', label=('ID pojazdu: '+str(i)))
+            plt.plot(_x_route[len(_x_route)-2:], _y_route[len(_y_route)-2:], c=color_helper, linestyle='-.')  # Powrot
 
         if separate_plots or i == len(routes_lists)-1:
             plt.grid()
             if separate_plots:
                 plt.title('Trasa śmieciarki nr ' + str(i))
             else:
-                # plt.legend(lista_legendy)
-                plt.legend()
+                if arrow:
+                    # plt.legend(lista_legendy)
+                    pass
+                else:
+                    plt.legend()
                 plt.title("Trasy smieciarek")
             plt.scatter([xy[0] for xy in xy_points], [xy[1] for xy in xy_points], c=point_color, marker=point_marker)  # Na koncu, aby punkty byly na wierzchu
             plt.show()
@@ -63,5 +73,5 @@ Trasy = [[0, 1, 2, 5, 6, 0],
          [0, 3, 4, 0]]
 
 
-show_routes(Trasy, Punkty, separate_plots=False)
+show_routes(Trasy, Punkty, separate_plots=True, arrow=True)
 
